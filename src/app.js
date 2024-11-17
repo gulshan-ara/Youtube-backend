b92c5b30022b952ae2285603f2c000441e02c83e
gulshan-ara <gulshan3015@gmail.com>
2024-11-17 16:30:12 +0600

    Added api error and api response block

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// enabling cors
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// for recieving json data
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+// for recieving urls
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+// for using static files like image/pdf etc
+app.use(express.static("public"));
+
+// cookieparser
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import { configDotenv } from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 configDotenv();
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Server is running at ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection Failed !!! ", error);
+  });
 
 // import express from "express";
 
