516f78d11a176bce2cce81cb6ff8789251a7f5be
gulshan-ara <gulshan3015@gmail.com>
2024-11-19 19:36:40 +0600

    Added user register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,75 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "OK",
+  // get user details from frontend / postman
+  // validation of data
+  // check if user already exists - check using username and email
+  // check for images
+  // check for avatar - required file
+  // upload images to cloudinary
+  // create user object to add in mongo db
+  // create entry in mongo db
+  // remove password and refreshToken field from response
+  // check for user creation
+  // return response or error
+
+  const { fullName, username, email, password } = req.body;
+
+  // validate data
+  if (
+    [fullName, username, email, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required.");
+  }
+
+  const existingUser = User.findOne({
+    $or: [
+      {
+        email,
+      },
+      { username },
+    ],
   });
+
+  if (existingUser) {
+    throw new ApiError(409, "User already exists.");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required.");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    username: username.toLowerCase(),
+    password,
+  });
+
+  // removing password and refreshToken from object so that it doesn't get sent to frontend
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user.");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfylly"));
 });
 
 export { registerUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser);
+// add file upload middleware
+userRouter.route("/register").post(
+  // injecting middleware
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default userRouter;
