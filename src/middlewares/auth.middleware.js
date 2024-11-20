acf5192fe9cb3587b83eeda6c1216e0b141d7aa1
gulshan-ara <gulshan3015@gmail.com>
2024-11-20 23:19:09 +0600

    Added logout middlware & controller

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      // retrieving from cookies
+      req.cookies?.accessToken ||
+      // retrieving from header where "Bearer <token>" is passed
+      // here replace method is removing the "Bearer " by replace method
+      req.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorised request.");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const user = User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid Access Token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
