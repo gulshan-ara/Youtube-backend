59500e6fcde70195bce1af5901a3089f58f99b78
gulshan-ara <gulshan3015@gmail.com>
2024-12-02 23:13:36 +0600

    Added refresh access token method

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -11,6 +11,8 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
       // retrieving from header where "Bearer <token>" is passed
       // here replace method is removing the "Bearer " by replace method
       req.header("Authorization")?.replace("Bearer ", "");
+    
+      console.log("token", token);
 
     if (!token) {
       throw new ApiError(401, "Unauthorised request.");
